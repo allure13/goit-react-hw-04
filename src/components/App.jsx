5bf8960066fe289d88999b9d5836119f5b4c23f9
Ihor Chepliaka <ihor.chepliaka@gmail.com>
2024-03-01 21:11:09 +0200

    add lib

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,7 @@
 import './App.css';
-import userData from '../userData.json';
-import Profile from './Profile/Profile';
-import friends from '../friends.json';
-import FriendList from './FriendList/FriendList';
-import TransactionHistory from './TransactionHistory/TransactionHistory';
-import transactions from '../transactions.json';
 
 const App = () => {
-  return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-
-      <FriendList friends={friends} />
-
-      <TransactionHistory items={transactions} />
-    </>
-  );
+  return <></>;
 };
 
 export default App;
